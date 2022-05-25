0902a2a6b6a4fe2696f289f22ad9d84f0dc76998
vaish33 <vaishnaviis@outlook.com>
2022-05-25 11:09:45 +0530

    Initial Commit

---
+++ Classes_and_objects.py
@@ -0,0 +1,10 @@
+# Class:
+# A class is a user-defined blueprint or prototype from which objects are created. 
+# Classes provide a means of bundling data and functionality together. 
+# Creating a new class creates a new type of object, allowing new instances of that type to be made. 
+# Properties and Methods form a central entity called Class
+# Class is an abstraction of some entity which contains common set of properties and methods
+
+# Example : Human is a class, 
+#           Name, Gender, Occupation are properties
+
