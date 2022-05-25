f08840056c0311d6d2a956c2435cbcd9d2028566
vaish33 <vaishnaviis@outlook.com>
2022-05-25 14:24:03 +0530

    Updated Program

--- Classes_and_objects.py
+++ Classes_and_objects.py
@@ -7,4 +7,32 @@
 
 # Example : Human is a class, 
 #           Name, Gender, Occupation are properties
+#           They perform some kind of work like speaking , do work , sleeping . So this speaking , do work , sleeping are the methods
+
+# Object : Its a specific instance of a class
+
+class Human:
+    def __init__(self, n, o):
+        self.name = n
+        self.occupation = o
+    
+    def do_work(self):
+        if self.occupation == "tennis player":
+            print(self.name , "plays tennis")
+        elif self.occupation == "actor":
+            print(self.name , "shoots film")
+    
+    def speaks(self):
+        print(self.name,"says how are you ?")
+
+
+tom = Human("tom cruise" , "actor")
+tom.do_work()
+tom.speaks()
+
+maria = Human("maria sharapova" , "tennis player")
+maria.do_work()
+maria.speaks()
+
+
 
